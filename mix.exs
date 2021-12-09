e22087b269b5b97392235cc94e02859b02211457
Tom Taylor <tom@tomtaylor.co.uk>
2021-12-09 11:30:17 +0000

    Set minimum Elixir version to 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Twittertex.Mixfile do
     [
       app: :twittertex,
       version: "0.3.0",
-      elixir: "~> 1.2",
+      elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
